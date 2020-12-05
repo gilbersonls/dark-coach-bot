51c8e02890a896a85630add6576422abf2983726
Gilberson Silva <gilbersonls@gmail.com>
2020-12-05 03:47:03 -0300

    update migrations

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,16 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
-export const bot = new TelegramBot(process.env.TELEGRAM_API_TOKEN, {
-  polling: process.env.NODE_ENV === 'development',
+const DEVELOPMENT = process.env.NODE_ENV === 'development';
+
+const bot = new TelegramBot(process.env.TELEGRAM_API_TOKEN, {
+  polling: DEVELOPMENT,
 });
 
-process.env.NODE_ENV === 'development' && console.info('telegram polling has started!');
-
 async function bootstrap() {
+  if (DEVELOPMENT) console.info('telegram polling has started!');
+  else await bot.setWebHook(`${process.env.PUBLIC_URL}/telegram/webhook`);
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
